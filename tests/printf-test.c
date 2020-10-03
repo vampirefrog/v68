94e0da21dabf17572b25434ae7ae7ef84bc17a23
vampirefrog <vampi@vampi.tech>
2020-10-03 16:40:16 +0300

    more printf test

--- tests/printf-test.c
+++ tests/printf-test.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
+
+#ifdef _WIN32
+#  ifdef _WIN64
+#    define PRI_SIZET PRIu64
+#    define PRI_SSIZET PRId64
+#  else
+#    define PRI_SIZET PRIu32
+#    define PRI_SSIZET PRId32
+#  endif
+#else
+#  define PRI_SIZET "zu"
+#  define PRI_SSIZET "zd"
+#endif
 
 int main(int argc, char **argv) {
-	uint8_t u8 = 1;
-	int8_t i8 = 2;
-	uint16_t u16 = 3;
-	int16_t i16 = 4;
-	uint32_t u32 = 5;
-	int32_t i32 = 6;
-	uint64_t u64 = 7;
-	int64_t i64 = 8;
-
-	size_t s = 7;
-	char c = 8;
-	unsigned char u = 9;
-	short sh = 10;
-	unsigned short ush = 11;
-	long l = 12;
-	unsigned long ul = 13;
-
-	int i = 14;
-	unsigned int ui = 15;
-
-	printf("uint8_t  %d\n", u8);
-	printf(" int8_t  %d\n", i8);
-	printf("uint16_t %d\n", u16);
-	printf(" int16_t %d\n", i16);
-	printf("uint32_t %d\n", u32);
-	printf(" int32_t %d\n", i32);
-	printf("uint64_t %d\n", u64);
-	printf(" int64_t %d\n", i64);
-
-	printf("size_t %d\n", s);
-
-	printf("char   %d\n", c);
-	printf("u char %d\n", u);
-	printf("short  %d\n", sh);
-	printf("u short %d\n", ush);
-	printf("long   %d\n", l);
-	printf("u long %d\n", ul);
-	printf("int    %d\n", i);
-	printf("unsigned int %d\n", ui);
+	uint8_t         u8 = __LINE__; printf(" uint8_t       %"PRIu8"\n",     u8 );
+	int8_t          i8 = __LINE__; printf("  int8_t       %"PRId8"\n",     i8 );
+	uint16_t       u16 = __LINE__; printf("uint16_t       %"PRIu16"\n",    u16);
+	int16_t        i16 = __LINE__; printf(" int16_t       %"PRId16"\n",    i16);
+	uint32_t       u32 = __LINE__; printf("uint32_t       %"PRIu32"\n",    u32);
+	int32_t        i32 = __LINE__; printf(" int32_t       %"PRId32"\n",    i32);
+	uint64_t       u64 = __LINE__; printf("uint64_t       %"PRIu64"\n",    u64);
+	int64_t        i64 = __LINE__; printf(" int64_t       %"PRId64"\n",    i64);
+	intmax_t         m = __LINE__; printf("intmax_t       %"PRIdMAX"\n",    m );
+	uintmax_t       um = __LINE__; printf("uintmax_t      %"PRIuMAX"\n",    um);
+	size_t           s = __LINE__; printf("size_t         %"PRI_SIZET"\n",   s);
+	ssize_t         ss = __LINE__; printf("ssize_t        %"PRI_SSIZET"\n", ss);
+	char             c = __LINE__; printf("char           %d\n",  c  );
+	unsigned char    u = __LINE__; printf("unsigned char  %d\n",  u  );
+	short           sh = __LINE__; printf("short          %d\n",  sh );
+	unsigned short ush = __LINE__; printf("unsigned short %d\n",  ush);
+	long             l = __LINE__; printf("long           %ld\n", l  );
+	unsigned long   ul = __LINE__; printf("unsigned long  %lu\n", ul );
+	int              i = __LINE__; printf("int            %d\n",  i  );
+	unsigned int    ui = __LINE__; printf("unsigned int   %d\n",  ui );
 
 	return 0;
 }
