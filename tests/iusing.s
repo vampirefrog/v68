18f1e6f06a825682aa907e7d55cd642a4c512d47
vampirefrog <vampi@vampi.tech>
2020-10-15 00:18:38 +0300

    iusing test: more verbose

--- tests/iusing.s
+++ tests/iusing.s
@@ -7,6 +7,14 @@ BUFSIZE equ 16
 STRWIDTH equ 12
 
 .text
+	pea.l (hello,pc)
+	DOS _PRINT
+	addq.l #4, sp
+
+	pea.l (result,pc)
+	DOS _PRINT
+	addq.l #4, sp
+
 	move.l #$7fffffff, d0
 	move.l #STRWIDTH, d1
 	sub.l #BUFSIZE, sp
@@ -19,12 +27,16 @@ STRWIDTH equ 12
 
 	add.l #BUFSIZE, sp
 
-	pea.l (crlf)
+	pea.l (resultEnd,pc)
 	DOS _PRINT
 	addq.l #4, sp
 
 	DOS _EXIT
 
 .data
-crlf:
-.dc.b $0d, $0a, $00
+hello:
+.dc.b '__IUSING test', $0d, $0a, $00
+result:
+.dc.b 'Result: "', $00
+resultEnd:
+.dc.b '"', $0d, $0a, $00
