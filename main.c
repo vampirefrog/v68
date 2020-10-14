b28e0008dd75fb790cd1d7936ca64efee4a6b9f5
vampirefrog <vampi@vampi.tech>
2020-10-15 00:16:55 +0300

    v68: color fix

--- main.c
+++ main.c
@@ -10,6 +10,17 @@
 #include <portaudio.h>
 #include "platform.h"
 
+#ifdef __MINGW32__
+#define NEED_COLOR_FIX
+#endif
+
+#ifdef NEED_COLOR_FIX
+
+#include <windows.h>
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
+#endif
+
 #include "v68.h"
 #include "v68io.h"
 #include "tools.h"
@@ -132,6 +143,17 @@ int parse_cmdline(int argc, char **argv) {
 }
 
 int main(int argc, char **argv, char **envp) {
+#ifdef NEED_COLOR_FIX
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (handle != INVALID_HANDLE_VALUE) {
+      DWORD mode = 0;
+        if (GetConsoleMode(handle, &mode)) {
+          mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+          SetConsoleMode(handle, mode);
+        }
+    }
+#endif
+
 	if(parse_cmdline(argc, argv)) {
 		fprintf(stderr, "Could not parse command line\n");
 		return -1;
